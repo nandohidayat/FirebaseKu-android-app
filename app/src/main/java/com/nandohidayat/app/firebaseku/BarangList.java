3e5cb363c1b54337d322525b8a793c464595fac1
Nando <mnandohidayat@gmail.com>
2019-01-02 03:00:33 +0700

    remove useless import

--- app/src/main/java/com/nandohidayat/app/firebaseku/BarangList.java
+++ app/src/main/java/com/nandohidayat/app/firebaseku/BarangList.java
@@ -1,9 +1,6 @@
 package com.nandohidayat.app.firebaseku;
 
 import android.app.Activity;
-import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
