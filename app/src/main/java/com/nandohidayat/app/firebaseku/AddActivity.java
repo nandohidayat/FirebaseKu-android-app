cc389f0469f86522554d8d9bd80bdb55e90362e5
Nando <mnandohidayat@gmail.com>
2019-01-02 04:02:20 +0700

    add update and delete

--- app/src/main/java/com/nandohidayat/app/firebaseku/AddActivity.java
+++ app/src/main/java/com/nandohidayat/app/firebaseku/AddActivity.java
@@ -1,15 +1,20 @@
 package com.nandohidayat.app.firebaseku;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddActivity extends AppCompatActivity {
     EditText editKdBrg;
@@ -22,6 +27,8 @@ public class AddActivity extends AppCompatActivity {
     Button buttonAdd;
     Button buttonDel;
 
+    String KdBrg;
+
     DatabaseReference databaseReference;
 
     @Override
@@ -30,6 +37,7 @@ public class AddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add);
 
         Intent intent = getIntent();
+        KdBrg = intent.getStringExtra(MainActivity.KD_BARANG);
 
         editKdBrg = findViewById(R.id.kdbrg);
         editNmBrg = findViewById(R.id.nmbrg);
@@ -41,17 +49,46 @@ public class AddActivity extends AppCompatActivity {
         buttonAdd = findViewById(R.id.btnadd);
         buttonDel = findViewById(R.id.btndel);
 
-        if(intent.getStringExtra(MainActivity.KD_BARANG).length() == 0) {
+        if(KdBrg.length() == 0) {
             databaseReference = FirebaseDatabase.getInstance().getReference("barangs");
             buttonDel.setVisibility(View.GONE);
         } else {
+            databaseReference = FirebaseDatabase.getInstance().getReference("barangs").child(KdBrg);
             buttonAdd.setText("Save");
+            editKdBrg.setEnabled(false);
+
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    Barang barang = dataSnapshot.getValue(Barang.class);
+                    editKdBrg.setText(barang.getKdbrg());
+                    editNmBrg.setText(barang.getNmbrg());
+                    editSatuan.setText(barang.getSatuan());
+                    editHrgBeli.setText(barang.getHrgbeli() + "");
+                    editHrgJual.setText(barang.getHrgjual() + "");
+                    editStok.setText(barang.getStok() + "");
+                    editStokMin.setText(barang.getStok_min() + "");
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
         }
 
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addBarang();
+            }
+        });
+
+        buttonDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.removeValue();
+                Toast.makeText(getApplicationContext(), "Delete Successful", Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -67,9 +104,13 @@ public class AddActivity extends AppCompatActivity {
 
         Barang barang = new Barang(KdBrg, NmBrg, Satuan, HrgBeli, HrgJual, Stok, StokMin);
 
-        databaseReference.child(KdBrg).setValue(barang);
-
-        Toast.makeText(this, "Successfully Added", Toast.LENGTH_LONG).show();
+        if(KdBrg.length() == 0) {
+            databaseReference.child(KdBrg).setValue(barang);
+            Toast.makeText(this, "Successfully Added", Toast.LENGTH_LONG).show();
+        } else {
+            databaseReference.setValue(barang);
+            Toast.makeText(this, "Change Saved", Toast.LENGTH_LONG).show();
+        }
 
         Intent intent = new Intent();
         setResult(1, intent);
