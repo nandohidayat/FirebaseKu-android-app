4a67a04a7f6df7546b92a06eedb14e7c33ddd875
Nando <mnandohidayat@gmail.com>
2019-01-01 23:35:21 +0700

    add action bar option

--- app/src/main/java/com/nandohidayat/app/firebaseku/MainActivity.java
+++ app/src/main/java/com/nandohidayat/app/firebaseku/MainActivity.java
@@ -2,12 +2,50 @@ package com.nandohidayat.app.firebaseku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String KD_BARANG = "com.nandohidayat.app.firebaseku.kdbrg";
+
+    ListView listView;
+
+    List<Barang> barangs;
+
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("barangs");
+
+        listView = (ListView) findViewById(R.id.list_barang);
+
+        barangs = new ArrayList<>();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_barang:
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
